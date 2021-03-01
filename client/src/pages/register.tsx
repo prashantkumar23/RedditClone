e3fda6d36f66178b532cf0d80068cd753359fff3
Prashant <blackhole.gamma@gmail.com>
2021-03-01 11:05:09 +0530

    updated login image path and added github action

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -48,7 +48,9 @@ export default function Register() {
 
       <div
         className="h-screen bg-center bg-cover w-36"
-        style={{ backgroundImage: "url('/images/1.jpg')" }}
+        style={{
+          backgroundImage: `url('${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/images/1.jpg')`,
+        }}
       ></div>
       <div className="flex flex-col justify-center pl-6">
         <div className="w-70">
